0ce6d32255f10ec10ca75ab23a8f2b88f00521db
msiddalingaiah <madhu@madhu.com>
2023-06-11 18:44:59 -0400

    Refactor, check for overlapping fields

--- Verilog/microcoded/roms/Generator.py
+++ Verilog/microcoded/roms/Generator.py
@@ -7,9 +7,36 @@ SEQ_OP_JUMP = 1
 SEQ_OP_CALL = 2
 SEQ_OP_RETURN = 3
 
+class MicroWord(object):
+    def __init__(self, fields, big_endian, seq_width):
+        self.fields = fields
+        self.big_endian = big_endian
+        self.seq_width = seq_width
+        self.word = 0
+        self.used_bits = 0
+
+    def update(self, field_name, value, lineNumber):
+        if field_name not in self.fields:
+            raise Exception(f'line {lineNumber}, No such field: {field_name}')
+        i1, i2 = self.fields[field_name]
+        width = abs(i1-i2)+1
+        mask = ~(-1 << width)
+        value = value & mask
+        if self.big_endian:
+            shift = self.seq_width - (i1+width)
+        else:
+            shift = i2
+        #print(f'{i1}:{i2} = {value}, seq_width: {self.seq_width}, width: {width}, shift: {shift}')
+        mask <<= shift
+        if mask & self.used_bits != 0:
+            raise Exception(f"line {lineNumber}: field '{field_name}' already assigned")
+        self.used_bits |= mask
+        value <<= shift
+        self.word |= value
+        return value
+
 class Generator(object):
     def __init__(self, tree):
-        self.output = []
         self.constants = { 'BIG':BIG_ENDIAN, 'LITTLE':LITTLE_ENDIAN }
         self.fields = {}
         self.procedures = {}
@@ -18,11 +45,12 @@ class Generator(object):
         self.pass1(tree)
         self.seq_width = self.constants['seq.width']
         self.big_endian = self.constants['seq.endian'] == BIG_ENDIAN
-        self.output = []
+        self.outputWords = []
         self.gen_stat_list(self.procedures['main'])
         for name, stat_list in self.procedures.items():
             if name != 'main':
-                self.proc_address[name] = self.gen_bit_field('seq.address', len(self.output))
+                word = MicroWord(self.fields, self.big_endian, self.seq_width)
+                self.proc_address[name] = word.update('seq.address', len(self.outputWords), 0)
                 self.gen_stat_list(stat_list)
         for address, name in self.patch.items():
             self.output[address] |= self.proc_address[name]
@@ -32,7 +60,8 @@ class Generator(object):
         a1, a2 = self.fields['seq.address']
         w = abs(a1-a2)+1
         word_count = 1 << w
-        output = self.output + ([0]*(word_count-len(self.output)))
+        words = [x.word for x in self.outputWords]
+        output = words + ([0]*(word_count-len(words)))
         with open(file_name, 'wt') as f:
             for value in output:
                 f.write(format.format(value))
@@ -76,63 +105,48 @@ class Generator(object):
 
     def gen_stat_list(self, stat_list):
         for stat in stat_list:
+            lineNumber = stat[0].value.lineNumber
             if stat[0].value.name == 'loop':
-                top = len(self.output)
+                top = len(self.outputWords)
                 self.gen_stat_list(stat[1])
-                jump = self.gen_bit_field('seq.op', SEQ_OP_JUMP) | self.gen_bit_field('seq.address', top)
-                self.output[-1] |= jump
+                self.outputWords[-1].update('seq.op', SEQ_OP_JUMP, lineNumber)
+                self.outputWords[-1].update('seq.address', top, lineNumber)
             elif stat[0].value.name == 'do':
-                top = len(self.output)
+                top = len(self.outputWords)
                 self.gen_stat_list(stat[1])
-                jump = self.gen_bit_field('seq.op', SEQ_OP_JUMP) | self.gen_bit_field('seq.address', top)
-                self.output[-1] |= jump
+                self.outputWords[-1].update('seq.op', SEQ_OP_JUMP, lineNumber)
+                self.outputWords[-1].update('seq.address', top, lineNumber)
             else:
                 self.gen_stat(stat)
 
-    def gen_bit_field(self, field_name, value):
-        i1, i2 = self.fields[field_name]
-        width = abs(i1-i2)+1
-        mask = ~(-1 << width)
-        value = value & mask
-        if self.big_endian:
-            shift = self.seq_width - (i1+width)
-        else:
-            shift = i2
-        #print(f'{i1}:{i2} = {value}, seq_width: {self.seq_width}, width: {width}, shift: {shift}')
-        return value << shift
-
-    # TODO leading and trailing jump operations can't overlap
-    # This should raise an error to avoid unexpected behavior
     def gen_stat(self, stat):
         # print('----')
-        word = 0
+        word = MicroWord(self.fields, self.big_endian, self.seq_width)
         op_index = 0
         while op_index < len(stat):
             op = stat[op_index]
             op_index += 1
+            lineNumber = op.value.lineNumber
             if op.value.name == '=':
                 field_name = op[0].value.value
-                if field_name not in self.fields:
-                    raise Exception(f'line {op[0].value.lineNumber}, No such field: {field_name}')
-                word |= self.gen_bit_field(field_name, self.eval(op[1]))
+                word.update(field_name, self.eval(op[1]), lineNumber)
             if op.value.name == 'return':
-                word |= self.gen_bit_field('seq.op', SEQ_OP_RETURN)
+                word.update('seq.op', SEQ_OP_RETURN, lineNumber)
             if op.value.name == 'call':
-                word |= self.gen_bit_field('seq.op', SEQ_OP_CALL)
+                word.update('seq.op', SEQ_OP_CALL, lineNumber)
                 op = stat[op_index]
                 op_index += 1
                 proc = op.value.value
                 self.patch[len(self.output)] = proc
             if op.value.name == 'while':
-                top = len(self.output)
-                self.output.append(word)
+                top = len(self.outputWords)
+                self.outputWords.append(word)
                 stat_list = stat[op_index]
                 op_index += 1
                 self.gen_stat_list(stat_list)
-                jump = self.gen_bit_field('seq.op', SEQ_OP_JUMP) | self.gen_bit_field('seq.address', top)
-                self.output[-1] |= jump
-                next = len(self.output)
-                jump = self.gen_bit_field('seq.address', next)
-                self.output[top] |= jump
+                self.outputWords[-1].update('seq.op', SEQ_OP_JUMP, lineNumber)
+                self.outputWords[-1].update('seq.address', top, lineNumber)
+                next = len(self.outputWords)
+                self.outputWords[top].update('seq.address', next, lineNumber)
                 return
-        self.output.append(word)
+        self.outputWords.append(word)
