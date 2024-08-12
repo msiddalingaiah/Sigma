78c3ecd0d953a306d4fef521c7dcc468f8c2a858
msiddalingaiah <madhu@madhu.com>
2024-08-11 23:43:50 -0400

    Shorten code

--- Verilog/microcoded/programs/init.asm
+++ Verilog/microcoded/programs/init.asm
@@ -4,40 +4,24 @@
         LI,1        0x1fffff
         LI,2        0
         LI,3        1
-        LI,1        'H'
-        WD,1        0
-        LI,1        'e'
-        WD,1        0
-        LI,1        'l'
-        WD,1        0
-        LI,1        'l'
-        WD,1        0
-        LI,1        'o'
-        WD,1        0
-        LI,1        '!'
-        WD,1        0
-        LI,1        '\n'
-        WD,1        0
+
+        LI,0        msg0
+        BAL,15      prnt
 
         LI,1        '0'
         LI,3        9
 lp1     AI,1        1
         WD,1        0
         BDR,3       lp1
-
         LI,1        '\n'
         WD,1        0
 
-        LI,2        0
-        LB,3        msg1,2
-        AI,2        1
-lp2     LB,1        msg1,2
-        AI,2        1
-        WD,1        0
-        BDR,3       lp2
+        LI,0        msg1
+        BAL,15      prnt
 
         LI,0        msg2
         BAL,15      prnt
+
         LI,0        msg3
         BAL,15      prnt
 
@@ -52,6 +36,7 @@ p0      LB,1        *0,2
         BDR,3       p0
         BCR,0       *15
 
+msg0    TEXTC       "Hello!\n"
 msg1    TEXTC       "Call me Ishmael...\n"
 msg2    TEXTC       "OK...\n"
 msg3    TEXTC       "Bye!\n"
